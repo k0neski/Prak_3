2c06c16378f3b57261f74c24329c4b121a728880
k0neski <125816238+k0neski@users.noreply.github.com>
2023-05-30 14:51:46 +0300

    Add files via upload

---
+++ Prak 3/Program.cs
@@ -0,0 +1,11 @@
+﻿/*12 вариант*/
+int a = 1, bo = 0, bn = 30, delb = 10;
+    double L = 0, b = 0, e = 0;
+
+for (int i = bo; i <= bn; i += delb)
+{
+    L += (24.2 * Math.Cos(Math.Pow(a, 2)) + (Math.Sin(a + b)))
+        / (Math.Cbrt(Math.Pow(e, a) - (Math.Pow(b, 3)) + (Math.Tan(b))));
+}
+
+Console.Write(L);
